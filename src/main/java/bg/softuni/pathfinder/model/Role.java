16e680924cc9dfd5da77bb4ed6370ce51214cbd2
hommm5 <hommm5@gmail.com>
2023-03-20 23:19:49 +0200

    building database

---
+++ src/main/java/bg/softuni/pathfinder/model/Role.java
@@ -0,0 +1,36 @@
+package bg.softuni.pathfinder.model;
+
+import bg.softuni.pathfinder.model.enums.UserRoles;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    private UserRoles name;
+
+    public Role() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Role setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public UserRoles getName() {
+        return name;
+    }
+
+    public Role setName(UserRoles name) {
+        this.name = name;
+        return this;
+    }
+}
---
+++ src/main/java/bg/softuni/pathfinder/model/User.java
@@ -0,0 +1,105 @@
+package bg.softuni.pathfinder.model;
+
+import bg.softuni.pathfinder.model.enums.UserLevel;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "fullname")
+    private String fullName;
+
+    private int age;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    @Enumerated(EnumType.STRING)
+    private UserLevel level;
+
+    public User() {
+        this.roles = new HashSet<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public User setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public User setFullName(String fullName) {
+        this.fullName = fullName;
+        return this;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public User setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public UserLevel getLevel() {
+        return level;
+    }
+
+    public User setLevel(UserLevel level) {
+        this.level = level;
+        return this;
+    }
+
+}
